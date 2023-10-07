71fc4291944b428d2ddeeac4ae1f1d53845f6ca6
Associate <Associate@roifmrwindt>
2023-10-07 20:30:24 +0530

    models added for trades and instruments

---
+++ src/app/models/instrument.model.ts
@@ -0,0 +1,9 @@
+export interface Instrument{    
+    instrumentId: string,
+    externalIdType: string,
+    externalId: string,
+    categoryId: string,
+    instrumentDescription: string,
+    minQuantity: number,
+    maxQuantity: number
+}
---
+++ src/app/models/portfolio.model.ts
@@ -0,0 +1,7 @@
+export interface Portfolio{
+    instrumentId: string,
+	description: string,
+	categoryId: string,
+	holdings: number,
+	price: number
+}
