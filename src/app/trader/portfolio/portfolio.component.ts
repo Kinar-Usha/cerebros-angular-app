7ebf6e20eea700adcfa0e64ced127c2103923777
Associate <Associate@roifmrwindt>
2023-10-16 13:26:41 +0530

    added a button download csv in portfolio

--- src/app/trader/portfolio/portfolio.component.ts
+++ src/app/trader/portfolio/portfolio.component.ts
@@ -44,5 +44,42 @@ export class PortfolioComponent implements OnInit {
     }
 
   }
+  downloadCSV() {
+    let csvData = this.ConvertToCSV(this.portfolio);
+    let a = document.createElement("a");
+    a.setAttribute('style', 'display:none;');
+    document.body.appendChild(a);
+    let blob = new Blob([csvData], { type: 'text/csv' });
+    let url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = 'portfolio.csv';
+    a.click();
+  }
+
+  ConvertToCSV(objArray: any) {
+    const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
+    let str = '';
+    let row = '';
+
+    for (let index in objArray[0]) {
+      // Now convert each value to string and comma-separated
+      row += index + ',';
+    }
+    row = row.slice(0, -1);
+    // append Label row with line break
+    str += row + '\r\n';
+
+    for (let i = 0; i < array.length; i++) {
+      let line = '';
+      for (let index in array[i]) {
+        if (line !== '') {
+          line += ',';
+        }
+        line += array[i][index];
+      }
+      str += line + '\r\n';
+    }
+    return str;
+  }
 
 }
